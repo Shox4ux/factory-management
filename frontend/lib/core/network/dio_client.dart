a46cba2dcfab93bf181a2fdbe23b5f3241f40780
Shox4ux <shoxruxu70@gmail.com>
2026-05-12 05:57:58 -0700

    Update API URL to new server and add Flutter build stage in Dockerfile

--- frontend/lib/core/network/dio_client.dart
+++ frontend/lib/core/network/dio_client.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 class DioClient {
   static Dio create() {
     final dio = Dio(BaseOptions(
-      baseUrl: 'http://94.183.182.130:8000',
+      baseUrl: 'http://104.253.175.53:8000',
       connectTimeout: const Duration(seconds: 10),
       receiveTimeout: const Duration(seconds: 15),
       headers: {'Content-Type': 'application/json'},
